c88907660f979bf00454441678b5ba254625792a
silver <silver36c@gmail.com>
2022-08-15 11:41:44 +0900

    FEAT: 추천받은 강의 엔티티 추가

---
+++ src/main/java/conatus/domain/entity/lecture/RecommendedLecture.java
@@ -0,0 +1,25 @@
+package conatus.domain.entity.lecture;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RecommendedLecture {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long userId;
+    private Long lectureId;
+}
