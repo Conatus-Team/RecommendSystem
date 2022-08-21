b1ddf3bea8c539f096e6ee5e4508350bb6bab773
silver <silver36c@gmail.com>
2022-08-21 13:28:35 +0900

    FEAT: DB 연결

--- conn/db_class.py
+++ conn/db_class.py
@@ -1,61 +1,31 @@
-from sqlalchemy import Column, TEXT, INT, BIGINT, TIMESTAMP
+from sqlalchemy import Column, TEXT, BIGINT, TIMESTAMP, INTEGER, String
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.types import JSON
 from sqlalchemy.dialects.mssql import TINYINT
 Base = declarative_base()
 
 
-
-"""
--- test_db.test definition
-
-CREATE TABLE `test` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `name` text NOT NULL,
-  `number` int NOT NULL,
-  `json_data` json DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-"""
-
-class Test(Base):
-    __tablename__ = 'test'
+class Hobby(Base):
+    __tablename__ = 'hobby'
     id = Column(BIGINT,nullable=False, autoincrement=True, primary_key=True)
-    name = Column(TEXT, nullable=False)
-    number = Column(INT, nullable=False)
-    json_data = Column(JSON)
+    name = Column(String)
+    place = Column(INTEGER)
+    compete = Column(INTEGER)
+    strategic = Column(INTEGER)
+    group = Column(INTEGER)
+    type = Column(INTEGER)
 
-
-"""
--- test_db.small_talk definition
-
-CREATE TABLE `small_talk` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `Q` text NOT NULL,
-  `A` text NOT NULL,
-  `embedding` json NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=47302 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-"""
-class SmallTalk(Base):
-    __tablename__ = 'chat_train'
+class HobbyPreference(Base):
+    __tablename__ = 'hobby_preference'
     id = Column(BIGINT,nullable=False, autoincrement=True, primary_key=True)
-    question = Column(TEXT, nullable=False)
-    answer = Column(TEXT, nullable=False)
-    embedding = Column(JSON, nullable=False)
-    # created_at = Column(TIMESTAMP)
-    # updated_at = Column(TIMESTAMP)
-    # is_deleted = Column(TINYINT)
+    user_id = Column(BIGINT)
+    tag = Column(String)
+    count = Column(INTEGER)
 
 
-# # JSON data
-# # Read a value
-# model.args['task']
-# # Read a value, cast to type
-# model.args['n'].as_integer()
-
-# # Filter by a value
-# db.session.query(Pick).filter(Pick.args['task'] = 'Test Task')
+class UserHobby(Base):
+    __tablename__ = 'user_hobby'
+    id = Column(BIGINT,nullable=False, autoincrement=True, primary_key=True)
+    user_id = Column(BIGINT)
+    hobby_id = Column(BIGINT)
+    hobby_name = Column(String)
--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from fastapi import Depends, FastAPI, Request
 from pydantic import BaseModel
-from conn.db_class import Test, SmallTalk
+from conn.db_class import *
 from conn.db_conn import engineconn
 from sqlalchemy import select
 from typing import List
 
-from routers import items, users, insert, chat, DB_connect_test
+from routers import rec_hobby
 
 app = FastAPI()
-app.include_router(users.router)
-app.include_router(items.router)
-app.include_router(insert.router)
-app.include_router(chat.router)
-app.include_router(DB_connect_test.router)
+app.include_router(rec_hobby.router)
+# app.include_router(items.router)
+# app.include_router(insert.router)
+# app.include_router(chat.router)
+# app.include_router(DB_connect_test.router)
 
 engine = engineconn()
 session = engine.sessionmaker()
@@ -37,24 +37,25 @@ def first_get():
 @app.get("/get")
 async def first_get():
     example = session.execute(
-    select(Test)
+    select(Hobby)
     ).scalars().all()
-    print(example)
     print(example[0])
     print(example[0].name)
     return example
 
-@app.post("/post")
-async def first_post(item:Item):
-    addMemo = Test(name=item.name, number=item.number, json_data=item.json_data)
-    session.add(addMemo)
-    session.commit()
-    print("addMemo")
-    return item
+# @app.post("/post")
+# async def first_post(item:Item):
+#     addMemo = Test(name=item.name, number=item.number, json_data=item.json_data)
+#     session.add(addMemo)
+#     session.commit()
+#     print("addMemo")
+#     return item
 
 # fastapi middleware, request state 에 db connection 심기
 @app.middleware("http")
 async def state_insert(request: Request, call_next):
-    request.state.db_conn = session
+    request.state.db_session = session
+    request.state.db_conn = engine.connection()
     response = await call_next(request)
     return response
+
